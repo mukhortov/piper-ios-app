e4f5d7ebea45f75c4d85c6328e8e1ce9b2278b45
Ihor Shevchuk <ihorshev@gmail.com>
2026-03-16 00:15:31 -0700

    Moved report type to flag

--- PiperApp/BuildScripts/Linting/SwiftLint/RunSwiftLint.sh
+++ PiperApp/BuildScripts/Linting/SwiftLint/RunSwiftLint.sh
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2026 Ihor Shevchuk
 
+echo "Start Linting..."
+
 if [ "$1" = "--fix" ] && [ -n "$CI_BUILD" ]; then
    echo "No automatic fixing on CI! Skipping"
    exit 0
@@ -17,10 +19,12 @@ fi
 if which mise >/dev/null; then
   mise install
   mise use swiftlint
-  mise exec -- swiftlint $1 --reporter ${REPORTER:-xcode} --config $(dirname "$0")/swiftlint.yml
+  mise exec -- swiftlint "$@" --config $(dirname "$0")/swiftlint.yml
 else
    # If mise was installed via brew it is needed to make symbolic lynk from mise in brew path to system path:
    # You may use next command to do this:
    # sudo ln -s /opt/homebrew/bin/mise /usr/local/bin/
    echo "warning: mise not installed"
 fi
+
+echo "Done Linting..."
